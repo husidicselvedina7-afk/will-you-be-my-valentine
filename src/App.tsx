b44eb5d70e6b5de0315c101297c4ccce67262f66
husidicselvedina7-afk <husidicselvedina7@gmail.com>
2026-02-03 09:00:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,7 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      Ne mogu 🙈",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -46,7 +31,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Hoćeš sa mnom na malicu u 10:00? 🥐☕</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
